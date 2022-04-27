ffafd55657440c32631866b8cbb8385f4df5df4f
Quentin-Desmettre <quentin.desmettre@epitech.eu>
2022-04-27 11:08:22 +0200

    [+] Libmy and gitignore

---
+++ lib/my/include/libmy.h
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2021
+** my
+** File description:
+** libmy.h
+*/
+
+#ifndef LIBMY_H
+    #define LIBMY_H
+    #include <stdlib.h>
+    #define init_with my_memset
+    #include "iomanip.h"
+    #include "linked_list.h"
+    #include "my_printf.h"
+    #include "others.h"
+    #include "strmanip.h"
+
+#endif
