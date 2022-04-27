565f6a827cfc858e0ab291444a5c6c8de9ab7f6a
Noa <noa.trachez@epitech.eu>
2022-04-27 11:12:27 +0200

    [+]Base file

---
+++ corewar/main.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2021
+** B-CPE-201-LIL-2-1-corewar-noa.trachez
+** File description:
+** main.c
+*/
+
+#include "unistd.h"
+
+int main(int ac, char **av)
+{
+    write(1, "Je suis le corewar\n", 19);
+    return (0);
+}
