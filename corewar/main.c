e298a6d0e314a2e4b1c39dd56ddfdfb5c4c086a2
Noa Trachez <noatrachez@MacBook-Pro-de-Noa.local>
2022-05-02 12:11:56 +0200

    [+]add help message to corewar

--- corewar/main.c
+++ corewar/main.c
@@ -6,9 +6,29 @@
 */
 
 #include "unistd.h"
+#include "corewar_include/op.h"
+#include "libmy.h"
+
+int help_message(char *name_binarie)
+{
+    write(1, "USAGE\n    ", 10);
+    write(1, name_binarie, my_strlen(name_binarie));
+    write(1, "[-dump nbr_cycle] [[-n prog_number] [-a load_address] prog_name] "
+    "... DESCRIPTION\n    -dump nbr_cycle dumps the memory after the nbr_cycle "
+    "execution (if the round isn’t already over) with the following format: 32 "
+    "bytes/line in hexadecimal (A0BCDEFE1DD3...)\n    -n prog_number sets the "
+    "next program’s number. By default, the first free number in the parameter "
+    "order\n    -a load_address sets the next program’s loading address. When "
+    "no address is specified, optimize the addresses so that the processes are "
+    "as far away from each other as possible. The addresses are MEM_SIZE "
+    "modulo.\n", 586);
+    return 0;
+}
 
 int main(int ac, char **av)
 {
-    write(1, "Je suis le corewar\n", 19);
+    if (ac == 2)
+        if (my_strcmp(av[1], "-h") == 0)
+            return (help_message(av[0]));
     return (0);
 }
