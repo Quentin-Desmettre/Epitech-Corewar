d55909c11802f2b5149ad053f229532b9a89812e
Clément <clement.vandeville@epitech.eu>
2022-05-13 14:50:21 +0200

    [+] Added operations instructions

--- corewar/instructions/i_operations.c
+++ corewar/instructions/i_operations.c
@@ -4,3 +4,17 @@
 ** File description:
 ** TODO: description
 */
+
+#include "op.h"
+
+int i_add(int arga, int argb, int argc)
+{
+    printf("add %d %d %d\n", arga, argb, argc);
+    return (0);
+}
+
+int i_sub(int arga, int argb, int argc)
+{
+    printf("sub %d %d %d\n", arga, argb, argc);
+    return (0);
+}
