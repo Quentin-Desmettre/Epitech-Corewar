e98cbabbc86239dee3bd1f3c902161a511e3a8af
Clément <clement.vandeville@epitech.eu>
2022-05-13 14:54:52 +0200

    [+] New .h file to store corewar core data

--- include/corewar_include/corewar.h
+++ include/corewar_include/corewar.h
@@ -5,7 +5,29 @@
 ** TODO: description
 */
 
-#ifndef B_CPE_201_LIL_2_1_COREWAR_NOA_TRACHEZ_COREWAR_H
-#define B_CPE_201_LIL_2_1_COREWAR_NOA_TRACHEZ_COREWAR_H
+#ifndef COREWAR_H
+    #define COREWAR_H
 
-#endif //B_CPE_201_LIL_2_1_COREWAR_NOA_TRACHEZ_COREWAR_H
+
+// Instructions
+int i_live(int arga);
+int i_zjmp(int arga);
+int i_fork(int arga);
+int i_lfork(int arga);
+int i_aff(int arga);
+int i_st(int arga, int argb);
+int i_sti(int arga, int argb, int argc);
+int i_ld(int arga, int argb);
+int i_ldi(int arga, int argb, int argc);
+int i_lld(int arga, int argb);
+int i_lldi(int arga, int argb);
+int i_add(int arga, int argb, int argc);
+int i_sub(int arga, int argb, int argc);
+int i_and(int arga, int argb, int argc);
+int i_or(int arga, int argb, int argc);
+int i_xor(int arga, int argb, int argc);
+
+// lib
+void my_memcpy(void *dest, void *source, size_t size);
+
+#endif /*COREWAR_H*/
