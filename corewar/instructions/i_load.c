d7b2caef8e628dc03c92136ecbee772db834c5a4
Clément <clement.vandeville@epitech.eu>
2022-05-13 14:50:55 +0200

    [+] Added memory load instructions

---
+++ corewar/instructions/i_load.c
@@ -0,0 +1,32 @@
+/*
+** EPITECH PROJECT, 2022
+** B_CPE_201_LIL_2_1_corewar_noa_trachez
+** File description:
+** TODO: description
+*/
+
+#include "op.h"
+
+int i_ld(int arga, int argb)
+{
+    printf("ld %d %d\n", arga, argb);
+    return (0);
+}
+
+int i_ldi(int arga, int argb, int argc)
+{
+    printf("ldi %d %d %d\n", arga, argb, argc);
+    return (0);
+}
+
+int i_lld(int arga, int argb)
+{
+    printf("lld %d %d\n", arga, argb);
+    return (0);
+}
+
+int i_lldi(int arga, int argb)
+{
+    printf("lldi %d %d\n", arga, argb);
+    return (0);
+}
