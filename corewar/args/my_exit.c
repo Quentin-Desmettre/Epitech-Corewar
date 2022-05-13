55cb0423245f506bdc267ab00d202ac7bd4403bc
Noa <tracheznoa@gmail.com>
2022-05-13 13:05:51 +0200

    [+]Dump option is now working, edit adress of champ arg

--- corewar/args/my_exit.c
+++ corewar/args/my_exit.c
@@ -31,19 +31,17 @@ void my_exit(int status)
     exit(status);
 }
 
-char *cor_strcpy(char *str1, const char *str2, int cc[2], size_t size)
+char *cor_strcpy(char *str1, const char *str2, const int cc[2], size_t size)
 {
     int index_str2 = 0;
 
-    for (size_t i = cc[0]; i <= size; i++, index_str2++) {
+    for (size_t i = cc[0]; index_str2 < size; i++, index_str2++) {
         if (i > MEM_SIZE)
             i = 0;
         if (str1[i] != '\0' && cc[1]) {
             free(str1);
             return (NULL);
         }
-        if ((size_t)index_str2 >= size)
-            break;
         str1[i] = str2[index_str2];
     }
     return (str1);
--- corewar/main.c
+++ corewar/main.c
@@ -23,6 +23,18 @@ int help_message(char *name_binarie)
     return 0;
 }
 
+void dump_print(char *map)
+{
+    int pos_index = 0;
+
+    for (pos_index = 0; pos_index < 6144; pos_index += 32) {
+        print("%-5X: ", pos_index);
+        for (int i = pos_index; i < pos_index + 32; i++)
+            print("%02hhX ", map[i]);
+        print("\n");
+    }
+}
+
 void set_map(champ_t *champ, char *map)
 {
     champ_t *save;
@@ -43,6 +55,7 @@ void set_map(champ_t *champ, char *map)
             exit(84);
         save = save->next;
     }
+    dump_print(map);
 }
 
 int main(int ac, char **av)
