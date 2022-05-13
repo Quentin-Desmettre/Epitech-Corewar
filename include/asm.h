5a759c906d30a5152953bee8031e47f356dfa69e
Quentin-Desmettre <quentin.desmettre@epitech.eu>
2022-05-13 23:26:00 +0200

    [+] All doc

--- include/asm.h
+++ include/asm.h
@@ -18,6 +18,29 @@
     #include "libmy.h"
     #include "op.h"
 
+    #pragma pack(push, 1)
+
+/**
+ * @struct command_data_t
+ * @brief Structure for unit tests purposes.
+ */
+typedef struct command_data {
+    char code;
+    char coding_byte;
+    uint8_t params[4];
+} command_data_t;
+
+/**
+ * @struct command_data2_t
+ * @brief Structure for unit tests purposes.
+ */
+typedef struct command_data2 {
+    char code;
+    uint8_t params[4];
+} command_data2_t;
+
+    #pragma pack(pop)
+
 /**
  * @struct command_t
  * @brief Structure to represent a command.
@@ -280,25 +303,104 @@ int check_name(FILE *f, char const *file, int *line);
  */
 void get_args(command_t *c, char **words);
 
+/**
+ * @brief Get the dir object
+ * 
+ * @param name 
+ * @param word 
+ * @param c 
+ * @param i 
+ */
 void get_dir(char *name, char *word, command_t *c, int i);
 void get_label(int is_index, command_t *c, char *word);
 void get_ind(command_t *c, char *word);
 void get_reg(char *word, command_t *c);
 void save_name(header_t *header, char *line);
 void get_comment(header_t *header, char *line);
+
+/**
+ * @brief Create a label object.
+ * @param name The name of the label.
+ * @param cmd The command it is linked to.
+ * @return A new label_t representing the label.
+ */
 label_t *create_label(char const *name, command_t *cmd);
+
+/**
+ * @brief Split the next line from a file.
+ * @param base_words The base word array
+ * @param f The file.
+ * @param nb_line A pointer to the number of lines read so far.
+ * @return A str array containing the new line, splitted.
+ */
 char **split_next_line(char ***base_words, FILE *f, int *nb_line);
+
+/**
+ * @brief Create a null command object.
+ * @param prev The previous command.
+ * @return A new command_t containing a null command.
+ */
 command_t *create_null_command(command_t *prev);
-command_t *create_command(char **words, command_t *prev);
+
+/**
+ * @brief Get all the data from a line.
+ * @param words The current words.
+ * @param buf The file buffer.
+ * @param line The current line.
+ */
 void get_data(char **words, file_buffer_t *buf, char *line);
+
+/**
+ * @brief Check if the argument given to the direct label is valid.
+ * @param arg The argument.
+ * @param err_mess A pointer to a string, to contain an error message.
+ * @return T_DIR if the label is valid, else T_ERROR.
+ */
 char check_direct_validity(char const *arg, char **err_mess);
+
+/**
+ * @brief Check if the given label has a copy.
+ * @param labels The label list.
+ * @param lab The label to check.
+ * @param except The label to except from check.
+ * @return 1 if it has a copy, else 0.
+ */
 int has_copy(list_t *labels, label_t *lab, list_t *except);
+
+/**
+ * @brief Check if there is 2 times the same label.
+ * @param labels The labels
+ * @return 1 if there is doubles, else 0.
+ */
 int has_double(list_t *labels);
+
+/**
+ * @brief Get the position of the label 'name'.
+ * @param labels The list of labels
+ * @param name The name of the label to find
+ * @param not_found A pointer to an integer,
+ * telling if we have found the command or not.
+ * @return The label pos. If it has not been found,
+ * returns 0 and set not_found to 1.
+ */
 int32_t get_label_pos(list_t *labels, char const *name, int *not_found);
+
+/**
+ * @brief Search and replace the labels of the given command.
+ * @param current The comma d in which the labels have to be changed.
+ * @param index_of_label The index of the label in the command's arguments
+ * @param labels The list of labels
+ * @return 0 if it failed, 1 if it succeeded.
+ */
 int search_for_label(command_t *current, int index_of_label, list_t *labels);
-int resolve_labels(file_buffer_t *buf);
+
+/**
+ * @brief Write a single command to fd.
+ * @param cmd The command to write
+ * @param fd The file descriptor in wich to write
+ * @return The number of byte effectively written.
+ */
 int write_command(command_t *cmd, int fd);
-int write_buffer(file_buffer_t *buf, char const *output);
 
 /**
  * @brief Add a 2-byte integer to the command's parameters
