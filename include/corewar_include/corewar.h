2ee5c850112269b12764273a897af0688420ccd0
Noa <tracheznoa@gmail.com>
2022-05-16 11:45:40 +0200

    [~].h files in corewar includes

--- include/corewar_include/corewar.h
+++ include/corewar_include/corewar.h
@@ -7,6 +7,9 @@
 
 #ifndef COREWAR_H
     #define COREWAR_H
+    #include "op.h"
+
+enum I_TYPE {EMPTY, REGISTER, DIRECT, INDIRECT};
 
 // ARGS
 typedef struct {
@@ -14,7 +17,88 @@ typedef struct {
     char type[3];
 } args_t;
 
-enum I_TYPE {EMPTY, REGISTER, DIRECT, INDIRECT};
+typedef struct param_champ_s {
+    int champ_nbr;
+    int nb_is_impose;
+    int adress;
+    int adress_impose;
+} param_champ_t;
+
+typedef struct champ_s{
+    char *name_champ;
+    char *instruction;
+    char registers[REG_NUMBER];
+    int pc;
+    int is_alive;
+    header_t header;
+    param_champ_t param;
+    struct champ_s *next;
+} champ_t;
+
+typedef struct {
+    int adress_act;
+    int adress_next;
+    int num_champ;
+    int index;
+    int num_impose[5];
+    int last_opt;
+    int dump_cycle;
+} param_argv_t;
+
+//main.c
+int help_message(char *name_binarie);
+void print_info_champ(champ_t *info_champ);
+
+//check args
+void check_dump(param_argv_t *param, char *arg);
+void check_num(param_argv_t *param, char *arg);
+void check_address(param_argv_t *param, char *arg);
+
+//create champ
+champ_t *init_node(param_argv_t *param, char *path_file);
+void append_champ(char *path_file, param_argv_t *param, champ_t **info_champ);
+void create_champ(char *path_file, param_argv_t *param, champ_t **info_champ);
+
+//check champ
+void delete_champ(champ_t **info_champ);
+int get_num_of_champ(champ_t **info_champ);
+void check_champ(int nb_to_change, champ_t **info_champ);
+
+//num champ
+void find_little_number(int valid_num[][4], int *nbr);
+void setup_valid_num(champ_t **info_champ, int valid_num[][4]);
+void set_champ_real_num(champ_t **info_champ, int valid_num[][4]);
+void change_all_nb_champ(champ_t **info_champ, int nb);
+int check_same_nbr(param_argv_t *param);
+
+//arg manage
+param_argv_t *init_value(void);
+int check_if_opt(param_argv_t *param, char *str, char *str_next, int *index);
+void check_argv(int *ac, char **av, int *dump_cycle, champ_t **info_champ);
+
+//endian
+void convert_endian(int *nbr);
+
+//header_champ
+void check_header(champ_t **info_champ, size_t size);
+void read_header(champ_t **info_champ, size_t size);
+void fill_header_champ(champ_t **info_champ);
+
+//sort_champ
+int find_little_but_higher(champ_t *champ, int int_compare);
+champ_t *create_nodes(champ_t *info, int champ);
+champ_t *append_champ_with_value(champ_t *sorted, int little, champ_t *all);
+champ_t *sort_my_list(champ_t *champ);
+
+//my_exit
+char *cor_strcpy(char *str1, const char *str2, const int cc[2], size_t size);
+
+//corewar
+void dump_print(char *map);
+char *set_map(champ_t *champ, char *map);
+void main_loop(char *map, champ_t *champions);
+void print_winner(champ_t *info_champ);
+void setup_game(int ac, char **av);
 
 // Instructions
 int i_live(int arga);
--- include/corewar_include/op.h
+++ include/corewar_include/op.h
@@ -21,7 +21,6 @@
     #include <unistd.h>
     #include <fcntl.h>
     #include "libmy.h"
-    #include "corewar.h"
 
 typedef char args_type_t;
 
@@ -56,6 +55,7 @@ typedef struct header_s {
     char comment[COMMENT_LENGTH + 1];
 } header_t;
 
+    #include "corewar.h"
     #define CYCLE_TO_DIE 1536
     #define CYCLE_DELTA 5
     #define NBR_LIVE 40
@@ -85,88 +85,5 @@ static const op_t op_tab[] = {
     {"aff", 1, {T_REG}, 16, 2, "aff"},
     {0, 0, {0}, 0, 0, 0}
 };
-
-typedef struct param_champ_s {
-    int champ_nbr;
-    int nb_is_impose;
-    int adress;
-    int adress_impose;
-} param_champ_t;
-
-typedef struct champ_s{
-    char *name_champ;
-    char *instruction;
-    char registers[REG_NUMBER];
-    int pc;
-    int is_alive;
-    header_t header;
-    param_champ_t param;
-    struct champ_s *next;
-} champ_t;
-
-typedef struct {
-    int adress_act;
-    int adress_next;
-    int num_champ;
-    int index;
-    int num_impose[5];
-    int last_opt;
-    int dump_cycle;
-} param_argv_t;
-
-//main.c
-int help_message(char *name_binarie);
-void print_info_champ(champ_t *info_champ);
-
-//check args
-void check_dump(param_argv_t *param, char *arg);
-void check_num(param_argv_t *param, char *arg);
-void check_address(param_argv_t *param, char *arg);
-
-//create champ
-champ_t *init_node(param_argv_t *param, char *path_file);
-void append_champ(char *path_file, param_argv_t *param, champ_t **info_champ);
-void create_champ(char *path_file, param_argv_t *param, champ_t **info_champ);
-
-//check champ
-void delete_champ(champ_t **info_champ);
-int get_num_of_champ(champ_t **info_champ);
-void check_champ(int nb_to_change, champ_t **info_champ);
-
-//num champ
-void find_little_number(int valid_num[][4], int *nbr);
-void setup_valid_num(champ_t **info_champ, int valid_num[][4]);
-void set_champ_real_num(champ_t **info_champ, int valid_num[][4]);
-void change_all_nb_champ(champ_t **info_champ, int nb);
-int check_same_nbr(param_argv_t *param);
-
-//arg manage
-param_argv_t *init_value(void);
-int check_if_opt(param_argv_t *param, char *str, char *str_next, int *index);
-void check_argv(int *ac, char **av, int *dump_cycle, champ_t **info_champ);
-
-//endian
-void convert_endian(int *nbr);
-
-//header_champ
-void check_header(champ_t **info_champ, size_t size);
-void read_header(champ_t **info_champ, size_t size);
-void fill_header_champ(champ_t **info_champ);
-
-//sort_champ
-int find_little_but_higher(champ_t *champ, int int_compare);
-champ_t *create_nodes(champ_t *info, int champ);
-champ_t *append_champ_with_value(champ_t *sorted, int little, champ_t *all);
-champ_t *sort_my_list(champ_t *champ);
-
-//my_exit
-char *cor_strcpy(char *str1, const char *str2, const int cc[2], size_t size);
-
-//corewar
-void dump_print(char *map);
-char *set_map(champ_t *champ, char *map);
-void main_loop(char *map, champ_t *champions);
-void print_winner(champ_t *info_champ);
-void setup_game(int ac, char **av);
 
 #endif
