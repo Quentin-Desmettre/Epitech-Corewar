9f5be4ca089dbd9a8c78ad8e2780dd50ff8409c2
Clément <clement.vandeville@epitech.eu>
2022-05-17 13:00:51 +0200

    [+] ld, ldi instructions added

--- corewar/instructions/i_load.c
+++ corewar/instructions/i_load.c
@@ -9,14 +9,21 @@
 
 int i_ld(int arg[3], champ_t *champ, char *arena)
 {
-    my_memcpy(&champ->registers[arg[1]],
-    arena + (champ->pc + (arg[0] % IDX_MOD)) % MEM_SIZE, 4);
+    memcpy_cor(&champ->registers[arg[1]], arena,
+    (champ->pc + (arg[0] % IDX_MOD)) % MEM_SIZE, 4);
     return (0);
 }
 
 int i_ldi(int arg[3], champ_t *champ, char *arena)
 {
-    print("ldi %d %d %d\n", arg[0], arg[1], arg[2]);
+    int sum = 0;
+
+    memcpy_cor(&sum, arena, ((champ->pc + arg[0]) % IDX_MOD) % MEM_SIZE,
+    IND_SIZE);
+    sum += arg[1];
+    memcpy_cor(&champ->registers[arg[2]], arena,
+    ((champ->pc + sum) % IDX_MOD) % MEM_SIZE, REG_SIZE);
+    champ->carry = !champ->carry;
     return (0);
 }
 
