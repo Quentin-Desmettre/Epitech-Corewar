624d7bec77a2762758a63afbc5372e2af8abfc83
Noa <noa.trachez@epitech.eu>
2022-05-22 19:03:54 +0200

    [+] cor file for tests

--- tests/corewar/test.c
+++ tests/corewar/test.c
@@ -683,3 +683,104 @@ Test (check_champ_func, check_champ_func)
     check_champ(&champ);
     cr_assert(champ->header.magic == COREWAR_EXEC_MAGIC);
 }
+
+Test (functionnal_test, check_functionnal_test, .init=cr_redirect_stdout)
+{
+    char *res[8] = {"../tests/corewar/cor_binary/42.cor",
+    "../tests/corewar/cor_binary/add.cor",
+    "../tests/corewar/cor_binary/aff.cor",
+    "../tests/corewar/cor_binary/and.cor",
+    "../tests/corewar/cor_binary/bigzork.cor",
+    "../tests/corewar/cor_binary/live.cor",
+    "../tests/corewar/cor_binary/lld.cor",
+    "../tests/corewar/cor_binary/loose.cor"};
+
+    for (int i = 0; i < 8; i++) {
+        char *av[3] = {"./corewar", res[0], res[i]};
+
+        setup_game(3, av);
+    }
+    char *av[5] = {"./corewar", res[0], res[1], "-dump", "500"};
+
+    setup_game(5, av);
+}
+
+Test (functionnal_test_double_prog_number, check_functionnal_test,
+.init=cr_redirect_stderr, .exit_code = 84)
+{
+    for (int i = 0; i < 8; i++) {
+        char *av[5] = {"./corewar", "-n", "2", "-n", "2"};
+        setup_game(5, av);
+    }
+}
+
+Test (functionnal_test_above_limit, check_functionnal_test,
+.init=cr_redirect_stderr, .exit_code = 84)
+{
+    char *res[8] = {"../tests/corewar/cor_binary/42.cor",
+    "../tests/corewar/cor_binary/add.cor",
+    "../tests/corewar/cor_binary/aff.cor",
+    "../tests/corewar/cor_binary/and.cor",
+    "../tests/corewar/cor_binary/bigzork.cor",
+    "../tests/corewar/cor_binary/live.cor",
+    "../tests/corewar/cor_binary/lld.cor",
+    "../tests/corewar/cor_binary/loose.cor"};
+    for (int i = 0; i < 8; i++) {
+        char *av[6] = {"./corewar", res[0], res[1], res[2], res[3], res[4]};
+        setup_game(6, av);
+    }
+}
+
+Test (functionnal_test_bad_option, check_functionnal_test,
+.init=cr_redirect_stderr, .exit_code = 84)
+{
+    char *res[8] = {"../tests/corewar/cor_binary/42.cor",
+    "../tests/corewar/cor_binary/add.cor",
+    "../tests/corewar/cor_binary/aff.cor",
+    "../tests/corewar/cor_binary/and.cor",
+    "../tests/corewar/cor_binary/bigzork.cor",
+    "../tests/corewar/cor_binary/live.cor",
+    "../tests/corewar/cor_binary/lld.cor",
+    "../tests/corewar/cor_binary/loose.cor"};
+
+    for (int i = 0; i < 8; i++) {
+        char *av[4] = {"./corewar", res[1], "-n", 0};
+        setup_game(3, av);
+    }
+}
+
+Test (functionnal_test_bad_option_address, check_functionnal_test,
+.init=cr_redirect_stderr, .exit_code = 84)
+{
+    char *res[8] = {"../tests/corewar/cor_binary/42.cor",
+    "../tests/corewar/cor_binary/add.cor",
+    "../tests/corewar/cor_binary/aff.cor",
+    "../tests/corewar/cor_binary/and.cor",
+    "../tests/corewar/cor_binary/bigzork.cor",
+    "../tests/corewar/cor_binary/live.cor",
+    "../tests/corewar/cor_binary/lld.cor",
+    "../tests/corewar/cor_binary/loose.cor"};
+
+    for (int i = 0; i < 8; i++) {
+        char *av[6] = {"./corewar", res[1], "-a", "5", "-a", "5"};
+        setup_game(6, av);
+    }
+}
+
+Test (functionnal_test_bad_prog_number, check_functionnal_test,
+.init=cr_redirect_stderr, .exit_code = 84)
+{
+    char *res[8] = {"../tests/corewar/cor_binary/42.cor",
+    "../tests/corewar/cor_binary/add.cor",
+    "../tests/corewar/cor_binary/aff.cor",
+    "../tests/corewar/cor_binary/and.cor",
+    "../tests/corewar/cor_binary/bigzork.cor",
+    "../tests/corewar/cor_binary/live.cor",
+    "../tests/corewar/cor_binary/lld.cor",
+    "../tests/corewar/cor_binary/loose.cor"};
+
+    for (int i = 0; i < 8; i++) {
+        char *av[7] = {"./corewar", "-n", "2", res[1], "-n", "2", res[0]};
+        setup_game(7, av);
+    }
+}
